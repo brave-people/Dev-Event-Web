0531bef5cd65e80b59db5f3f0ee09c5a00461e52
ohh31 <saewon412@gmail.com>
2022-04-04 23:17:36 +0900

    feat: base api url 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    BASE_SERVER_URL: "https://dev-brave-people.o-r.kr/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
