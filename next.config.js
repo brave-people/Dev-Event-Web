21544e64bc24b4677d8828441d8d1eee6180809b
ohh31 <saewon412@gmail.com>
2022-04-26 23:03:16 +0900

    feat: svg 아이콘 버튼

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   env: {
     BASE_SERVER_URL: 'https://dev-brave-people.o-r.kr',
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
