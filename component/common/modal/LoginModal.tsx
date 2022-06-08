23d2440181e54fe4ab0ca08b467b14935b8d3432
ohh31 <saewon412@gmail.com>
2022-06-08 20:13:17 +0900

    feat : sns 로그인 연결

--- component/common/modal/LoginModal.tsx
+++ component/common/modal/LoginModal.tsx
@@ -7,14 +7,20 @@ import GoogleIcon from 'public/icon/google.svg';
 import KakaoIcon from 'public/icon/kakao.svg';
 import GithubIcon from 'public/icon/github.svg';
 import NaverIcon from 'public/icon/naver.svg';
+import router from 'next/router';
 
 const cx = classNames.bind(style);
 
 function LoginModal({ isOpen, onClick }: any) {
+  const requestLogin = async (type: String) => {
+    const url = `${process.env.BASE_SERVER_URL}/auth/login/${type}`;
+    router.push(url);
+  };
+
   return (
     <Modal isOpen={isOpen} className={cx('modal')} overlayClassName={cx('overlay')} onRequestClose={onClick}>
       <button className={cx('modal__close-button')} onClick={onClick}>
-        <MdClose size={45} />
+        <MdClose size={35} />
       </button>
       <div className={cx('modal__inner')}>
         <span className={cx('modal__title')}>
@@ -22,19 +28,39 @@ function LoginModal({ isOpen, onClick }: any) {
         </span>
 
         <div className={cx('login-form')}>
-          <button className={cx('login-form__button', 'kakao')}>
+          <button
+            className={cx('login-form__button', 'kakao')}
+            onClick={() => {
+              requestLogin('kakao');
+            }}
+          >
             <KakaoIcon />
             <span> 카카오톡 로그인</span>
           </button>
-          <button className={cx('login-form__button', 'google')}>
+          <button
+            className={cx('login-form__button', 'google')}
+            onClick={() => {
+              requestLogin('google');
+            }}
+          >
             <GoogleIcon />
             <span> 구글 로그인</span>
           </button>
-          <button className={cx('login-form__button', 'github')}>
+          <button
+            className={cx('login-form__button', 'github')}
+            onClick={() => {
+              requestLogin('github');
+            }}
+          >
             <GithubIcon />
             <span> github 로그인</span>
           </button>
-          <button className={cx('login-form__button', 'naver')}>
+          <button
+            className={cx('login-form__button', 'naver')}
+            onClick={() => {
+              requestLogin('naver');
+            }}
+          >
             <NaverIcon />
             <span>네이버 로그인</span>
           </button>
