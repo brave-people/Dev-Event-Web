d75b8fbac8ca88684d2db1033f8f7c305db9ad06
ohh31 <saewon412@gmail.com>
2022-06-09 23:10:17 +0900

    feat : 토큰 유무로 로그인 상태 확인 및 컴포넌트 조건 추가

---
+++ lib/context/auth.tsx
@@ -0,0 +1,41 @@
+import { Token } from 'model/auth';
+import React, { createContext, ReactNode, useState } from 'react';
+
+interface AuthContext {
+  isLoggedIn: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const defaultValue: AuthContext = {
+  isLoggedIn: false,
+  login: () => {},
+  logout: () => {},
+};
+
+const AuthContext = createContext(defaultValue);
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const login = () => {
+    setLoggedIn(true);
+  };
+
+  const logout = () => {
+    setLoggedIn(false);
+  };
+
+  const contextValue = {
+    isLoggedIn,
+    login,
+    logout,
+  };
+  return (
+    <>
+      <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+    </>
+  );
+};
+
+export { AuthContext, AuthProvider };
