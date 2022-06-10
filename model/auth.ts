69f81eff410b4da29e844f1ad4d5d454e18c35d7
ohh31 <saewon412@gmail.com>
2022-06-11 03:29:10 +0900

    feat : auth model (commit: e3123ff)

---
+++ model/auth.ts
@@ -0,0 +1,13 @@
+export type AuthType = 'GOOGLE';
+
+export interface User {
+  auth_type_enum: AuthType;
+  email: string;
+  register_date: string;
+  username: string;
+}
+
+export interface Token {
+  access_token: string;
+  refresh_token: string;
+}
