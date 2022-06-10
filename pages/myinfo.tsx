e3123fffbacb86aef19952426e2ec7c141ddfd7e
ohh31 <saewon412@gmail.com>
2022-06-11 00:01:22 +0900

    feat :내 정보 조회 swr 연결

--- pages/myinfo.tsx
+++ pages/myinfo.tsx
@@ -6,11 +6,14 @@ import classNames from 'classnames/bind';
 import style from 'styles/Myinfo.module.scss';
 import OutlineButton from 'component/common/buttons/OutlineButton';
 import DeleteAccountModal from 'component/common/modal/DeleteAccountModal';
+import { useUser } from 'lib/hooks/useSWR';
+import dayjs from 'dayjs';
 
 const cn = classNames.bind(style);
 
 const MyInfo = () => {
   const [DeleteAccountModalIsOpen, setDeleteAccountIsOpen] = useState(false);
+  const { user, isLoading, isError } = useUser();
 
   return (
     <div className={cn('info-container')}>
@@ -25,15 +28,19 @@ const MyInfo = () => {
         <div className={cn('info-form')}>
           <div className={cn('info-form__item')}>
             <span>이름</span>
-            <input className={cn('input', 'size--small')} disabled></input>
+            <input className={cn('input', 'size--small')} disabled value={user?.username}></input>
           </div>
           <div className={cn('info-form__item')}>
             <span>이메일</span>
-            <input className={cn('input', 'size--regular')} disabled></input>
+            <input className={cn('input', 'size--regular')} disabled value={user?.email}></input>
           </div>
           <div className={cn('info-form__item')}>
             <span>가입일</span>
-            <input className={cn('input', 'size--small')} disabled></input>
+            <input
+              className={cn('input', 'size--small')}
+              disabled
+              value={dayjs(user?.register_date).format('YYYY. MM. DD')}
+            ></input>
           </div>
         </div>
         <div className={cn('delete-button')}>
