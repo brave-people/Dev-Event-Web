0ba776b0436a57a5c85f196eec1a6d3f9b825001
ohh31 <saewon412@gmail.com>
2022-06-14 00:00:47 +0900

    feat : 검색결과, 월별 리스트 찜 추가, 삭제기능

--- component/events/MonthlyEventList.tsx
+++ component/events/MonthlyEventList.tsx
@@ -4,22 +4,94 @@ import type { ReactElement } from 'react';
 import classNames from 'classnames/bind';
 import style from 'styles/Home.module.scss';
 import { useRouter } from 'next/router';
-import { useMonthlyEvent } from 'lib/hooks/useSWR';
+import { useMonthlyEvent, useMyEvent } from 'lib/hooks/useSWR';
 import Item from 'component/common/item/Item';
+import dayjs from 'dayjs';
+import { Event } from 'model/event';
+import { mutate } from 'swr';
 import { createMyEventApi } from 'lib/api/post';
+import { deleteMyEventApi } from 'lib/api/delete';
 
 const cn = classNames.bind(style);
 
 const MonthlyEventList = () => {
   const router = useRouter();
+  const paramByOld = { filter: 'OLD' };
+  const paramByFuture = { filter: 'FUTURE' };
   const param = { year: Number(router.query.year), month: Number(router.query.month) };
+
   const { monthlyEvent, isLoading, isError } = useMonthlyEvent({
     param: param,
   });
+  const { myEvent: myOldEvent, isLoading: isMyOldEventLoading, isError: isMyOldEventError } = useMyEvent(paramByOld);
+  const {
+    myEvent: myFutureEvent,
+    isLoading: isMyFutureEventLoading,
+    isError: isMyFutureEventError,
+  } = useMyEvent(paramByFuture);
+
+  const checkEventDone = ({ endDate }: { endDate: string }) => {
+    const todayDate = dayjs();
+    const eventDate = dayjs(endDate);
+    return eventDate.diff(todayDate, 'day') > 0 ? false : true;
+  };
+
+  const onClickFavoriteOldEvent = async ({ item }: { item: Event }) => {
+    if (myOldEvent) {
+      const favoriteId = getFavoriteOldEventId({ id: item.id });
+
+      if (favoriteId === 0) {
+        const result = await createMyEvent({ eventId: item.id });
+      } else {
+        const result = await deleteMyEvent({ favoriteId: favoriteId });
+      }
+      mutate([`/front/v1/favorite/events`, paramByOld]);
+    }
+  };
+
+  const onClickFavoriteFutureEvent = async ({ item }: { item: Event }) => {
+    if (myFutureEvent) {
+      const favoriteId = getFavoriteFutureEventId({ id: item.id });
+
+      if (favoriteId === 0) {
+        const result = await createMyEvent({ eventId: item.id });
+      } else {
+        const result = await deleteMyEvent({ favoriteId: favoriteId });
+      }
+      mutate([`/front/v1/favorite/events`, paramByFuture]);
+    }
+  };
+
+  const getFavoriteOldEventId = ({ id }: { id: string }) => {
+    if (myOldEvent) {
+      const result = myOldEvent.find((item) => {
+        return item.dev_event.id === id;
+      });
+      return result ? result.favorite_id : 0;
+    }
+
+    return 0;
+  };
+
+  const getFavoriteFutureEventId = ({ id }: { id: string }) => {
+    if (myFutureEvent) {
+      const result = myFutureEvent.find((item) => {
+        return item.dev_event.id === id;
+      });
+      return result ? result.favorite_id : 0;
+    }
+    return 0;
+  };
 
   const createMyEvent = async ({ eventId }: { eventId: String }) => {
     const result = await createMyEventApi(`/front/v1/favorite/events/${eventId}`, {
       eventId: Number(eventId),
+    });
+  };
+
+  const deleteMyEvent = async ({ favoriteId }: { favoriteId: number }) => {
+    const result = await deleteMyEventApi(`/front/v1/favorite/events/${favoriteId}`, {
+      favoriteId: favoriteId,
     });
   };
 
@@ -38,9 +110,22 @@ const MonthlyEventList = () => {
                     <Item
                       key={item.id}
                       data={item}
-                      isFavorite={false}
-                      onClickFavorite={() => {
-                        createMyEvent({ eventId: item.id });
+                      isEventDone={() => {
+                        return checkEventDone({ endDate: item.end_date_time });
+                      }}
+                      isFavorite={({ filter }: { filter: string }) => {
+                        if (filter === 'OLD') {
+                          return getFavoriteOldEventId({ id: item.id }) !== 0 ? true : false;
+                        } else {
+                          return getFavoriteFutureEventId({ id: item.id }) !== 0 ? true : false;
+                        }
+                      }}
+                      onClickFavorite={({ filter }: { filter: string }) => {
+                        if (filter === 'OLD') {
+                          return onClickFavoriteOldEvent({ item: item });
+                        } else {
+                          return onClickFavoriteFutureEvent({ item: item });
+                        }
                       }}
                     />
                   }
