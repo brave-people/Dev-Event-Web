a5d99967829ee0f28c4a8752cfd2d3e95b3bc5e9
ohh31 <saewon412@gmail.com>
2022-06-17 02:49:40 +0900

    refactor : 로고 이미지 png에서 svg로 수정

--- component/common/layout/header/Header.tsx
+++ component/common/layout/header/Header.tsx
@@ -2,13 +2,13 @@ import classNames from 'classnames/bind';
 import style from './Header.module.scss';
 import React, { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import TextButton from 'component/common/buttons/TextButton';
 import SearchModal from 'component/common/modal/SearchModal';
 import LoginModal from 'component/common/modal/LoginModal';
 import SearchIcon from 'public/icon/search_outlined_regular.svg';
 import { AuthContext } from 'context/auth';
 import Profile from './Profile';
+import Logo from 'public/logo/logo.svg';
 
 const cn = classNames.bind(style);
 
@@ -23,7 +23,7 @@ function Header() {
       <div className={cn('header__inner')}>
         <Link href="/" passHref>
           <div className={cn('header__logo')}>
-            <Image src="/logo/logo.png" alt="logo" layout="fill" quality={100}></Image>
+            <Logo />
           </div>
         </Link>
         <div className={cn('header__buttons')}>
