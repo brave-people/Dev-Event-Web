72dd081a76420cb1531a61fdfe6b56ba79b1503d
ohh31 <saewon412@gmail.com>
2022-06-18 00:18:49 +0900

    fix : 엔터 이벤트에만 검색되도록 수정

--- component/common/modal/SearchModal.tsx
+++ component/common/modal/SearchModal.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './SearchModal.module.scss';
 import Modal from 'react-modal';
 import { FiSearch } from 'react-icons/fi';
@@ -8,19 +8,38 @@ import router from 'next/router';
 const cx = classNames.bind(style);
 
 function SearchModal({ isOpen, onClick }: any) {
-  const onSubmit = (event: any) => {
-    const keyword = event.target.value;
-    if (keyword) {
-      router.replace(`/events?keyword=${keyword}`);
-    } else {
-      router.replace(`/events`);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    if (!isOpen) {
+      setInput('');
     }
+  }, [isOpen]);
+
+  const onSubmit = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (event.code == 'Enter') {
+      if (input) {
+        router.replace(`/events?keyword=${input}`);
+      } else {
+        router.replace(`/events`);
+      }
+    }
+  };
+
+  const onChangeKeyword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
   };
 
   return (
     <Modal isOpen={isOpen} className={cx('modal')} overlayClassName={cx('overlay')} onRequestClose={onClick}>
       <div className={cx('modal__inner')}>
-        <input className={cx('search-input')} placeholder="어떤 행사를 찾고 계세요?" onKeyPress={onSubmit}></input>
+        <input
+          className={cx('search-input')}
+          placeholder="어떤 행사를 찾고 계세요?"
+          onChange={onChangeKeyword}
+          value={input}
+          onKeyPress={onSubmit}
+        ></input>
       </div>
     </Modal>
   );
