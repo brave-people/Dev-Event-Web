b7023e5ddb7d999c22562569ef137c06a6879151
ohh31 <saewon412@gmail.com>
2022-06-22 22:15:58 +0900

    fix : export 명령어 제거함으로 이미지 로더 삭제

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['brave-people-3.s3.ap-northeast-2.amazonaws.com'],
-    loader: 'akamai',
-    path: '/',
   },
   env: {
     BASE_SERVER_URL: 'https://dev-brave-people.o-r.kr',
