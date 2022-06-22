06bb9a4e1719f47bf31b45083ac7a1120e4ea57d
ohh31 <saewon412@gmail.com>
2022-06-22 21:58:49 +0900

    build : netflify 배포설정

--- next.config.js
+++ next.config.js
@@ -2,11 +2,15 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['brave-people-3.s3.ap-northeast-2.amazonaws.com'],
+    loader: 'akamai',
+    path: '/',
   },
   env: {
     BASE_SERVER_URL: 'https://dev-brave-people.o-r.kr',
   },
-
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
