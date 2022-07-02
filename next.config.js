f48b033071e45def535e7720c327cfe86ee9457f
ohh31 <saewon412@gmail.com>
2022-07-02 13:49:42 +0900

    build : 환경변수 설정

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     domains: ['brave-people-3.s3.ap-northeast-2.amazonaws.com'],
   },
   env: {
-    BASE_SERVER_URL: 'https://dev-brave-people.o-r.kr',
+    BASE_SERVER_URL:
+      process.env.NODE_ENV === 'production' ? 'https://real-brave-people.o-r.kr' : 'https://dev-brave-people.o-r.kr',
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
