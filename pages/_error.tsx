72a31104623f8cdd9b66b227bb530138740adce3
ohh31 <saewon412@gmail.com>
2022-07-07 01:48:57 +0900

    feat : 에러화면

---
+++ pages/_error.tsx
@@ -0,0 +1,28 @@
+import Layout from 'component/common/layout';
+import { NextPage, NextPageContext } from 'next';
+import { ReactElement } from 'react';
+
+interface Props {
+  statusCode?: number;
+}
+
+const Error = ({ statusCode }: { statusCode: Props }) => {
+  return (
+    <div className="error">
+      <span>
+        {statusCode} : {statusCode === 404 ? '페이지를 찾을 수 없습니다!' : '페이지를 불러오는 중에 문제가 발생했어요!'}
+      </span>
+    </div>
+  );
+};
+
+Error.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
