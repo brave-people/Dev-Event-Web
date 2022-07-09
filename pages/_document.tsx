4536dd88f9d3d95ca5d6764e885fc4784053af71
ohh31 <saewon412@gmail.com>
2022-07-09 23:25:16 +0900

    chore: 카카오톡 링크 공유를 위한 메타데이터 설정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            property="og:image"
+            content="https://drive.google.com/uc?export=download&id=1-Jqapt5h4XtxXQbgX07kI3ipgk3V6ESE"
+          />
+          <meta property="og:title" content="Dev Event" />
+          <meta property="og:description" content="개발자 {웨비나, 컨퍼런스, 해커톤} 행사를 알려드립니다." />
           <meta name="google-site-verification" content="nuMWgo5LJBkwv_VMLcsP3dS6eHzyyGcxKvnwVY9p5Vk" />
           <meta name="naver-site-verification" content="846c898c7464c31de7f1d43f7c8d6aabab12daf1" />
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
