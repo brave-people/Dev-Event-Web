3f1d3e5cbaeb14df8ddac05bd6fc639a356fceb6
ohh31 <saewon412@gmail.com>
2022-08-17 22:39:00 +0900

    refactor: 이벤트 리스트 컴포넌트로 분리 (#38)

--- component/events/MonthlyEventList.tsx
+++ component/events/MonthlyEventList.tsx
@@ -1,159 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Layout from 'component/common/layout';
 import type { ReactElement } from 'react';
 import classNames from 'classnames/bind';
 import style from 'styles/Home.module.scss';
 import { useRouter } from 'next/router';
-import { useMonthlyEvent, useMyEvent } from 'lib/hooks/useSWR';
-import Item from 'component/common/item/Item';
-import dayjs from 'dayjs';
-import { Event } from 'model/event';
-import { mutate } from 'swr';
-import { createMyEventApi } from 'lib/api/post';
-import { deleteMyEventApi } from 'lib/api/delete';
-import LoginModal from 'component/common/modal/LoginModal';
-import { AuthContext } from 'context/auth';
+import { useMonthlyEvent } from 'lib/hooks/useSWR';
 import { MdClose } from 'react-icons/md';
 import { ThreeDots } from 'react-loader-spinner';
-import * as ga from 'lib/utils/gTag';
-import ShareModal from 'component/common/modal/ShareModal';
+import List from 'component/common/list/list';
 
 const cn = classNames.bind(style);
 
 const MonthlyEventList = () => {
   const router = useRouter();
-  const paramByOld = { filter: 'OLD' };
-  const paramByFuture = { filter: 'FUTURE' };
-  const param = { year: Number(router.query.year), month: Number(router.query.month) };
-  const authContext = React.useContext(AuthContext);
-  const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
-  const [shareModalIsOpen, setShareModalIsOpen] = useState(false);
-  const [sharedEvent, setSharedEvent] = useState({});
 
-  const handleShareInMobileSize = (data: Event) => {
-    setSharedEvent(data);
-    setShareModalIsOpen(true);
-  };
+  const param = { year: Number(router.query.year), month: Number(router.query.month) };
 
   const { monthlyEvent, isError } = useMonthlyEvent({
     param: param,
   });
-  const { myEvent: myOldEvent, isError: isMyOldEventError } = useMyEvent(paramByOld, authContext.isLoggedIn);
-  const { myEvent: myFutureEvent, isError: isMyFutureEventError } = useMyEvent(paramByFuture, authContext.isLoggedIn);
 
   if (isError) {
     return <div className={cn('null-container')}>이벤트 정보를 불러오는데 문제가 발생했습니다!</div>;
   }
-  if (isMyOldEventError || isMyFutureEventError) {
-    return <div className={cn('null-container')}>내 이벤트 정보를 불러오는데 문제가 발생했습니다!</div>;
-  }
-
-  const checkEventNew = ({ createdDate }: { createdDate: string }) => {
-    const todayDate = dayjs();
-    const createDate = dayjs(createdDate);
-
-    return createDate.diff(todayDate, 'day') < 1 && createDate.diff(todayDate, 'day') > -1 ? true : false;
-  };
-  const checkEventDone = ({ endDate }: { endDate: string }) => {
-    const todayDate = dayjs().set('hour', 0).set('minute', 0).set('second', 0).set('millisecond', 0);
-    const eventDate = dayjs(endDate).set('hour', 0).set('minute', 0).set('second', 0).set('millisecond', 0);
-    return eventDate.diff(todayDate, 'day') > 0 ||
-      (eventDate.diff(todayDate, 'day') === 0 && eventDate.get('day') === todayDate.get('day'))
-      ? false
-      : true;
-  };
-
-  const onClickFavoriteOldEvent = async ({ item }: { item: Event }) => {
-    if (myOldEvent) {
-      const favoriteId = getFavoriteOldEventId({ id: item.id });
-
-      if (favoriteId === 0) {
-        const filteredEvent = myOldEvent.concat({ favorite_id: favoriteId, dev_event: item });
-        mutate([`/front/v1/favorite/events`, paramByOld], filteredEvent, false);
-        const result = await createMyEvent({ eventId: item.id });
-      } else {
-        const filteredEvent = myOldEvent.filter((event) => event.favorite_id !== favoriteId);
-        mutate([`/front/v1/favorite/events`, paramByOld], [...filteredEvent], false);
-
-        const result = await deleteMyEvent({ favoriteId: favoriteId });
-      }
-      mutate([`/front/v1/favorite/events`, paramByOld]);
-    }
-  };
-
-  const onClickFavoriteFutureEvent = async ({ item }: { item: Event }) => {
-    if (myFutureEvent) {
-      const favoriteId = getFavoriteFutureEventId({ id: item.id });
-
-      if (favoriteId === 0) {
-        const filteredEvent = myFutureEvent.concat({ favorite_id: favoriteId, dev_event: item });
-        mutate([`/front/v1/favorite/events`, paramByFuture], filteredEvent, false);
-        const result = await createMyEvent({ eventId: item.id });
-      } else {
-        const filteredEvent = myFutureEvent.filter((event) => event.favorite_id !== favoriteId);
-        mutate([`/front/v1/favorite/events`, paramByFuture], [...filteredEvent], false);
-        const result = await deleteMyEvent({ favoriteId: favoriteId });
-      }
-      mutate([`/front/v1/favorite/events`, paramByFuture]);
-    }
-  };
-
-  const getFavoriteOldEventId = ({ id }: { id: string }) => {
-    if (myOldEvent) {
-      const result = myOldEvent.find((item) => {
-        return item.dev_event.id === id;
-      });
-      return result ? result.favorite_id : 0;
-    }
-
-    return 0;
-  };
-
-  const getFavoriteFutureEventId = ({ id }: { id: string }) => {
-    if (myFutureEvent) {
-      const result = myFutureEvent.find((item) => {
-        return item.dev_event.id === id;
-      });
-      return result ? result.favorite_id : 0;
-    }
-    return 0;
-  };
-
-  const createMyEvent = async ({ eventId }: { eventId: String }) => {
-    if (eventId) {
-      const result = await createMyEventApi(`/front/v1/favorite/events/${eventId}`, {
-        eventId: Number(eventId),
-      });
-      if (result.status_code === 200 && result.status === 'FRONT_FAVORITE_201_01') {
-        return 'SUCCESS';
-      }
-    } else {
-      alert('이벤트 정보가 없습니다!');
-    }
-    ga.event({
-      action: 'web_event_관심행사추가버튼클릭',
-      event_category: 'web_event',
-      event_label: '관심행사',
-    });
-  };
-
-  const deleteMyEvent = async ({ favoriteId }: { favoriteId: number }) => {
-    if (favoriteId) {
-      const result = await deleteMyEventApi(`/front/v1/favorite/events/${favoriteId}`, {
-        favoriteId: favoriteId,
-      });
-      if (result.status_code === 200 && result.status === 'FRONT_FAVORITE_200_01') {
-        return 'SUCCESS';
-      }
-    } else {
-      alert('이벤트 정보가 없습니다!');
-    }
-    ga.event({
-      action: 'web_event_관심행사삭제버튼클릭',
-      event_category: 'web_event',
-      event_label: '관심행사',
-    });
-  };
 
   return (
     <>
@@ -171,49 +40,7 @@ const MonthlyEventList = () => {
         </div>
         {monthlyEvent && !isError ? (
           monthlyEvent.length !== 0 ? (
-            <div className={cn('section__list__items')}>
-              {monthlyEvent.map((item: any) => {
-                return (
-                  <div className={cn('wrapper')}>
-                    {
-                      <Item
-                        key={item.id}
-                        data={item}
-                        isEventNew={() => {
-                          return checkEventNew({ createdDate: item.create_date_time });
-                        }}
-                        isEventDone={() => {
-                          return checkEventDone({ endDate: item.end_date_time });
-                        }}
-                        isFavorite={({ filter }: { filter: string }) => {
-                          if (authContext.isLoggedIn) {
-                            if (filter === 'OLD') {
-                              return getFavoriteOldEventId({ id: item.id }) !== 0 ? true : false;
-                            } else {
-                              return getFavoriteFutureEventId({ id: item.id }) !== 0 ? true : false;
-                            }
-                          } else {
-                            return false;
-                          }
-                        }}
-                        onClickFavorite={({ filter }: { filter: string }) => {
-                          if (authContext.isLoggedIn) {
-                            if (filter === 'OLD') {
-                              return onClickFavoriteOldEvent({ item: item });
-                            } else {
-                              return onClickFavoriteFutureEvent({ item: item });
-                            }
-                          } else {
-                            setLoginModalIsOpen(true);
-                          }
-                        }}
-                        onClickShareInMobileSize={handleShareInMobileSize}
-                      />
-                    }
-                  </div>
-                );
-              })}
-            </div>
+            <List data={monthlyEvent} />
           ) : (
             <div className={cn('null-container')}>아직 조건에 맞는 개발자 행사가 없어요 📂</div>
           )
@@ -223,8 +50,6 @@ const MonthlyEventList = () => {
           </div>
         )}
       </div>
-      <LoginModal isOpen={loginModalIsOpen} onClick={() => setLoginModalIsOpen(false)}></LoginModal>
-      <ShareModal isOpen={shareModalIsOpen} onClick={() => setShareModalIsOpen(false)} data={sharedEvent}></ShareModal>
     </>
   );
 };
--- component/events/ScheduledEventList.tsx
+++ component/events/ScheduledEventList.tsx
@@ -1,52 +1,20 @@
-import React, { useState } from 'react';
-import { useMyEvent, useScheduledEvents } from 'lib/hooks/useSWR';
+import React from 'react';
+import { useScheduledEvents } from 'lib/hooks/useSWR';
 import { EventResponse, Event } from 'model/event';
 import classNames from 'classnames/bind';
 import style from 'styles/Home.module.scss';
-import Item from 'component/common/item/Item';
-import { createMyEventApi } from 'lib/api/post';
-import { deleteMyEventApi } from 'lib/api/delete';
-import { mutate } from 'swr';
 import dayjs from 'dayjs';
-import { AuthContext } from 'context/auth';
-import LoginModal from 'component/common/modal/LoginModal';
 import { ThreeDots } from 'react-loader-spinner';
-import * as ga from 'lib/utils/gTag';
-import ShareModal from 'component/common/modal/ShareModal';
+import List from 'component/common/list/list';
 
 const cn = classNames.bind(style);
 
 const ScheduledEventList = () => {
-  const paramByOld = { filter: 'OLD' };
-  const paramByFuture = { filter: 'FUTURE' };
-  const authContext = React.useContext(AuthContext);
-  const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
-  const [shareModalIsOpen, setShareModalIsOpen] = useState(false);
-  const [sharedEvent, setSharedEvent] = useState({});
-
-  const handleShareInMobileSize = (data: Event) => {
-    setSharedEvent(data);
-    setShareModalIsOpen(true);
-  };
-
   const { scheduledEvents, isError } = useScheduledEvents();
-
-  const { myEvent: myOldEvent, isError: isMyOldEventError } = useMyEvent(paramByOld, authContext.isLoggedIn);
-  const { myEvent: myFutureEvent, isError: isMyFutureEventError } = useMyEvent(paramByFuture, authContext.isLoggedIn);
 
   if (isError) {
     return <div className={cn('null-container')}>이벤트 정보를 불러오는데 문제가 발생했습니다!</div>;
   }
-  if (isMyOldEventError || isMyFutureEventError) {
-    return <div className={cn('null-container')}>내 이벤트 정보를 불러오는데 문제가 발생했습니다!</div>;
-  }
-
-  const checkEventNew = ({ createdDate }: { createdDate: string }) => {
-    const todayDate = dayjs();
-    const createDate = dayjs(createdDate);
-
-    return createDate.diff(todayDate, 'day') < 1 && createDate.diff(todayDate, 'day') > -1 ? true : false;
-  };
 
   const checkEventDone = ({ endDate }: { endDate: string }) => {
     const todayDate = dayjs().set('hour', 0).set('minute', 0).set('second', 0).set('millisecond', 0);
@@ -55,98 +23,6 @@ const ScheduledEventList = () => {
       (eventDate.diff(todayDate, 'day') === 0 && eventDate.get('day') === todayDate.get('day'))
       ? false
       : true;
-  };
-
-  const onClickFavoriteOldEvent = async ({ item }: { item: Event }) => {
-    if (myOldEvent) {
-      const favoriteId = getFavoriteOldEventId({ id: item.id });
-
-      if (favoriteId === 0) {
-        const filteredEvent = myOldEvent.concat({ favorite_id: favoriteId, dev_event: item });
-        const result = await createMyEvent({ eventId: item.id });
-        mutate([`/front/v1/favorite/events`, paramByOld], filteredEvent, false);
-      } else {
-        const filteredEvent = myOldEvent.filter((event) => event.favorite_id !== favoriteId);
-        const result = await deleteMyEvent({ favoriteId: favoriteId });
-        mutate([`/front/v1/favorite/events`, paramByOld], [...filteredEvent], false);
-      }
-      mutate([`/front/v1/favorite/events`, paramByOld]);
-    }
-  };
-
-  const onClickFavoriteFutureEvent = async ({ item }: { item: Event }) => {
-    if (myFutureEvent) {
-      const favoriteId = getFavoriteFutureEventId({ id: item.id });
-
-      if (favoriteId === 0) {
-        const filteredEvent = myFutureEvent.concat({ favorite_id: favoriteId, dev_event: item });
-        const result = await createMyEvent({ eventId: item.id });
-        mutate([`/front/v1/favorite/events`, paramByFuture], filteredEvent, false);
-      } else {
-        const filteredEvent = myFutureEvent.filter((event) => event.favorite_id !== favoriteId);
-        const result = await deleteMyEvent({ favoriteId: favoriteId });
-        mutate([`/front/v1/favorite/events`, paramByFuture], [...filteredEvent], false);
-      }
-      mutate([`/front/v1/favorite/events`, paramByFuture]);
-    }
-  };
-
-  const getFavoriteOldEventId = ({ id }: { id: string }) => {
-    if (myOldEvent) {
-      const result = myOldEvent.find((item) => {
-        return item.dev_event.id === id;
-      });
-      return result ? result.favorite_id : 0;
-    }
-
-    return 0;
-  };
-
-  const getFavoriteFutureEventId = ({ id }: { id: string }) => {
-    if (myFutureEvent) {
-      const result = myFutureEvent.find((item) => {
-        return item.dev_event.id === id;
-      });
-      return result ? result.favorite_id : 0;
-    }
-    return 0;
-  };
-
-  const createMyEvent = async ({ eventId }: { eventId: String }) => {
-    if (eventId) {
-      const result = await createMyEventApi(`/front/v1/favorite/events/${eventId}`, {
-        eventId: Number(eventId),
-      });
-
-      if (result.status_code === 200 && result.status === 'FRONT_FAVORITE_201_01') {
-        return 'SUCCESS';
-      }
-    } else {
-      alert('이벤트 정보가 없습니다!');
-    }
-    ga.event({
-      action: 'web_event_관심행사추가버튼클릭',
-      event_category: 'web_event',
-      event_label: '관심행사',
-    });
-  };
-
-  const deleteMyEvent = async ({ favoriteId }: { favoriteId: number }) => {
-    if (favoriteId) {
-      const result = await deleteMyEventApi(`/front/v1/favorite/events/${favoriteId}`, {
-        favoriteId: favoriteId,
-      });
-      if (result.status_code === 200 && result.status === 'FRONT_FAVORITE_201_01') {
-        return 'SUCCESS';
-      }
-    } else {
-      alert('이벤트 정보가 없습니다!');
-    }
-    ga.event({
-      action: 'web_event_관심행사삭제버튼클릭',
-      event_category: 'web_event',
-      event_label: '관심행사',
-    });
   };
 
   return (
@@ -163,60 +39,13 @@ const ScheduledEventList = () => {
                     <div className={cn('section__list__title')}>
                       <span>{`${event.metadata.year}년 ${event.metadata.month}월`}</span>
                     </div>
-                    <div className={cn('section__list__items')}>
-                      {event &&
-                        event.dev_event
-                          .filter((item) => checkEventDone({ endDate: item.end_date_time }) === false)
-                          // .concat(
-                          //   event.dev_event.filter((item) => checkEventDone({ endDate: item.end_date_time }) === true)
-                          // )
-                          .map((item: Event) => {
-                            return (
-                              <div className={cn('wrapper')}>
-                                <Item
-                                  key={item.id}
-                                  data={item}
-                                  isEventNew={() => {
-                                    return checkEventNew({ createdDate: item.create_date_time });
-                                  }}
-                                  isEventDone={() => {
-                                    return checkEventDone({ endDate: item.end_date_time });
-                                  }}
-                                  isFavorite={({ filter }: { filter: string }) => {
-                                    if (authContext.isLoggedIn) {
-                                      if (filter === 'OLD') {
-                                        return getFavoriteOldEventId({ id: item.id }) !== 0 ? true : false;
-                                      } else {
-                                        return getFavoriteFutureEventId({ id: item.id }) !== 0 ? true : false;
-                                      }
-                                    } else {
-                                      return false;
-                                    }
-                                  }}
-                                  onClickShareInMobileSize={handleShareInMobileSize}
-                                  onClickFavorite={({ filter }: { filter: string }) => {
-                                    if (authContext.isLoggedIn) {
-                                      if (filter === 'OLD') {
-                                        return onClickFavoriteOldEvent({ item: item });
-                                      } else {
-                                        return onClickFavoriteFutureEvent({ item: item });
-                                      }
-                                    } else {
-                                      setLoginModalIsOpen(true);
-                                    }
-                                  }}
-                                />
-                              </div>
-                            );
-                          })}
-                    </div>
+                    <List
+                      data={
+                        event &&
+                        event.dev_event.filter((item) => checkEventDone({ endDate: item.end_date_time }) === false)
+                      }
+                    />
                   </div>
-                  <LoginModal isOpen={loginModalIsOpen} onClick={() => setLoginModalIsOpen(false)}></LoginModal>
-                  <ShareModal
-                    isOpen={shareModalIsOpen}
-                    onClick={() => setShareModalIsOpen(false)}
-                    data={sharedEvent}
-                  ></ShareModal>
                 </>
               );
             })
