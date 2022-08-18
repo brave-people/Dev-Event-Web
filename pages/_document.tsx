faf86051b37e16bbf8a43653f1b491a48b54423d
ohh31 <saewon412@gmail.com>
2022-08-19 00:29:30 +0900

    feat : 사이트맵 및 메타데이터 추가 (#26)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="title" content="Dev Event - 개발자 행사는 모두 데브이벤트 웹에서!" />
+          <meta name="robots" content="index,follow" />
           <meta
             name="description"
             content="데브이벤트 웹에서 개발자 행사를 놓치지 마세요! 개발자를 위한 {웨비나, 컨퍼런스, 해커톤, 네트워킹} 소식을 알려드립니다."
@@ -19,6 +21,7 @@ class MyDocument extends Document {
             name="keywords"
             content="데브이벤트 웹, Dev Event, 데브이벤트, 개발자 행사, 용감한 친구들, Web, 웹, 개발자, 이벤트, 행사, 웨비나, 컨퍼런스, 해커톤, 네트워킹, IT"
           />
+          <meta property="og:type" content="website" />
           <meta
             property="og:image"
             content="https://drive.google.com/uc?export=download&id=1-Jqapt5h4XtxXQbgX07kI3ipgk3V6ESE"
@@ -32,6 +35,7 @@ class MyDocument extends Document {
           <meta property="og:site_name" content="Dev Event" />
           <meta name="google-site-verification" content="nuMWgo5LJBkwv_VMLcsP3dS6eHzyyGcxKvnwVY9p5Vk" />
           <meta name="naver-site-verification" content="846c898c7464c31de7f1d43f7c8d6aabab12daf1" />
+          <link rel="canonical" href="https://dev-event.vercel.app"></link>
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
