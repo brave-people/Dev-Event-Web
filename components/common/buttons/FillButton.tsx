69ac3ecee485ad828770e9866feff0d45589fcb8
Resister-boy <hassanpumped17@gmail.com>
2023-06-14 13:55:06 +0900

    fix: applicate FillButton type

--- components/common/buttons/FillButton.tsx
+++ components/common/buttons/FillButton.tsx
@@ -1,12 +1,23 @@
 import classNames from 'classnames/bind';
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import style from './FillButton.module.scss';
+import getIconByName from 'lib/utils/iconUtil';
 
 const cx = classNames.bind(style);
 
-function FillButton({ onClick, size = 'regular', label, color }: any) {
+type FillButtonProps = {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  label: string;
+  color: string;
+  size?: string;
+  icon?: string;
+  iconStyle?: string;
+}
+
+function FillButton({ onClick, size = 'regular', label, color, icon, iconStyle }: FillButtonProps) {
   return (
     <button className={cx('button', `size--${size}`, color)} onClick={onClick}>
+      {getIconByName('plus', iconStyle, '#ffffff')}
       {label}
     </button>
   );
