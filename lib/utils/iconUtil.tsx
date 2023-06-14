f2b5cecffbc4af892a81c35d59645d631a9c71a3
Resister-boy <hassanpumped17@gmail.com>
2023-06-14 14:26:39 +0900

    feat: add new icons

--- lib/utils/iconUtil.tsx
+++ lib/utils/iconUtil.tsx
@@ -1,14 +1,22 @@
-import { PlusIcon } from "components/icons";
+import { PlusIcon, SearchIcon } from "components/icons";
 import React from "react";
 
 function getIconByName(icon: string, className: string | undefined, color: string) {
-  if (icon === "plus")
+  if (icon === "plus") {
     return (
       <PlusIcon 
         className={className}
         color={color}
       />
     )
+  } else if (icon === 'search') {
+    return (
+      <SearchIcon
+        className={className}
+        color={color}
+      />
+    )
+  }
 }
 
 
