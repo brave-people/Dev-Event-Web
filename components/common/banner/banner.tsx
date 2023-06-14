4ac92cf2429db1fc52201d15e482968fce81b839
Resister-boy <hassanpumped17@gmail.com>
2023-06-14 13:07:46 +0900

    faet: edit banner

--- components/common/banner/banner.tsx
+++ components/common/banner/banner.tsx
@@ -12,7 +12,7 @@ function Banner() {
         <br /> 모두 Dev Event 웹에서
       </h1>
       <h3 className={cn('banner__desc')}>진행 중인 행사부터 종료된 행사까지, 놓치지 마세요! </h3>
-      <Image src="/default/banner_img.png" layout="fill" className={cn('banner__image')} />
+      <div className={cn('banner__image')} />
     </div>
   );
 }
