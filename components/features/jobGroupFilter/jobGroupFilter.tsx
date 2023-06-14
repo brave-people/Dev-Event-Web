70649a4ca5ada29096d4aace62f20d9858bb1589
Resister-boy <hassanpumped17@gmail.com>
2023-06-14 12:57:09 +0900

    feat: add event filter tag

---
+++ components/features/jobGroupFilter/jobGroupFilter.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { getTagsApi } from "lib/api/handler";
+import { TagResponse } from "model/tag";
+import classNames from "classnames/bind";
+import style from './jobGroupFilter.module.scss'
+import Tag from "components/common/tag/Tag";
+
+const cx = classNames.bind(style);
+
+function TagListByJobGroup() {
+  const [tagList, setTagList] = useState<TagResponse[] | undefined>(undefined);
+  const fetchTagList = async () => {
+    const result = await getTagsApi("/front/v1/events/tags");
+    console.log(result);
+    setTagList(result);
+  }
+  useEffect(() => {
+    fetchTagList();
+  }, []);
+  return (
+    <div className={cx('taglist')}>
+      {tagList?.map((tag) => {
+        return (
+          <Tag
+            key={tag.id}
+            tagName={tag.tag_name}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default TagListByJobGroup;
