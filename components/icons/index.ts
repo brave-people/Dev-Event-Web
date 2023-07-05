0f0871f8c34e93ebc8016ba032b0f3875f36080c
Resister-boy <hassanpumped17@gmail.com>
2023-07-05 18:49:54 +0900

    feat: add filled plus icon

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,10 +2,12 @@ import PlusIcon from "./PlusIcon";
 import SearchIcon from "./SearchIcon";
 import RightArrowIcon from "./RightArrowIcon";
 import LeftArrowIcon from "./LeftArrowIcon";
+import FilledPlusIcon from "./FilledPlusIcon";
 
 export {
   PlusIcon,
   SearchIcon,
   RightArrowIcon,
-  LeftArrowIcon
+  LeftArrowIcon,
+  FilledPlusIcon
 }
