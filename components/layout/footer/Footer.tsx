a4443b840b5075a517161cb8109a0d76b2e2ba1a
Resister-boy <hassanpumped17@gmail.com>
2023-07-11 20:47:38 +0900

    feat: applicate new design

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,20 +1,37 @@
 import classNames from 'classnames/bind';
-import Link from 'next/link';
 import style from './Footer.module.scss';
+import Logo from 'components/common/logo/Logo';
+import { GithubIcon, NaverWhaleIcon } from 'components/icons';
+import Link from 'next/link';
 
 const cn = classNames.bind(style);
 
 function Footer() {
   return (
     <footer className={cn('footer')}>
-      <span>
-        {' '}
-        ⓒ 2022.{' '}
-        <Link href="https://github.com/brave-people">
-          <a target="_blank">용감한 친구들 with 남송리 삼번지</a>
-        </Link>{' '}
-        all rights reserved.
-      </span>
+      <div className={cn('footer__column')}>
+        <Logo />
+        <div className={cn('footer__desc')}>
+          개발자를 위한 행사 정보, 데브이벤트
+        </div>
+        <div className={cn('footer__copyright')}>
+          ⓒ 2022. 용감한 친구들 with 남송리 삼번지 all rights reserved.
+        </div>
+      </div>
+      <div className={cn('footer__row')}>
+        <Link href={"https://github.com/brave-people"}>
+          <a target='_blank'>
+            <GithubIcon
+              className='github__icon'
+            />
+          </a>
+        </Link>
+        <Link href={"/event"}>
+          <a target='_blank'>
+            <NaverWhaleIcon />
+          </a>
+        </Link>
+      </div>
     </footer>
   );
 }
