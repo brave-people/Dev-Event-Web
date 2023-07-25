3a65fedd78494c9d184ca8228d8f33a2ed06f501
Resister-boy <hassanpumped17@gmail.com>
2023-07-25 20:23:37 +0900

    feat: applicate responsive design

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Link from 'next/link';
 import LoginModal from 'components/common/modal/LoginModal';
 import { AuthContext } from 'context/auth';
@@ -8,6 +8,7 @@ import Profile from './Profile';
 import Logo from 'components/common/logo/Logo';
 import Login from 'components/features/login/Login'
 import ThemeToggle from 'components/features/ThemeToggle/ThemeToggle';
+import NoticeModal from 'components/common/modal/NoticeModal';
 
 const cn = classNames.bind(style);
 
@@ -17,6 +18,7 @@ function Header() {
   const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
   return (
     <header className={cn('header')}>
+      <NoticeModal />
       <div className={cn('header__inner')}>
         <Link href="/" passHref>
           <div className={cn('header__logo')}>
@@ -24,6 +26,9 @@ function Header() {
           </div>
         </Link>
         <div className={cn('header__buttons')}>
+          <div className={cn('toggle__container')}>
+            <ThemeToggle />
+          </div>
           <span className={cn('wrapper')}>
             {authContext.isLoggedIn ? (
               <Profile />
@@ -33,7 +38,6 @@ function Header() {
               />
             )}
           </span>
-          <ThemeToggle />
         </div>
       </div>
       <LoginModal isOpen={loginModalIsOpen} onClose={() => setLoginModalIsOpen(false)}></LoginModal>
