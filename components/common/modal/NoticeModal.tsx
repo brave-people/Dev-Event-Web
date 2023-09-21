92b91066235715438f5defc001b8baa23ce49db2
leedoopal <1233wsh@gmail.com>
2023-09-21 20:50:53 +0900

    #78 상단띠 쿠키 적용, 상단띠에 따른 배너 스타일 수정

--- components/common/modal/NoticeModal.tsx
+++ components/common/modal/NoticeModal.tsx
@@ -1,39 +1,42 @@
-import React, { useContext } from "react"
-import Image from "next/image"
-import classNames from "classnames/bind"
-import style from 'components/common/modal/NoticeModal.module.scss'
-import { DeleteIcon } from "components/icons"
-import { WindowContext } from "context/window"
+import style from 'components/common/modal/NoticeModal.module.scss';
+import { DeleteIcon } from 'components/icons';
+import { WindowContext } from 'context/window';
+import Cookie from 'js-cookie';
+import { useContext, useEffect } from 'react';
+import classNames from 'classnames/bind';
+import Image from 'next/image';
 
-const cn = classNames.bind(style)
+const DEV_EVENT_NOTICE = 'dev-event-notice';
+
+const cn = classNames.bind(style);
 
 function NoticeModal() {
   const { isNotice, windowTheme, handleIsNotice } = useContext(WindowContext);
-  
-  const handleModal = () => {
-    handleIsNotice(false)
-  }
+
+  const hideModal = () => {
+    handleIsNotice(false);
+    Cookie.set(DEV_EVENT_NOTICE, 'true', { expires: 90 });
+  };
+
+  useEffect(() => {
+    if (Cookie.get(DEV_EVENT_NOTICE) === 'true') {
+      handleIsNotice(false);
+    }
+  }, []);
+
   return (
-    <div className={cn('notice', !isNotice && 'status--delete', windowTheme ? "notice--light" : "notice--dark")}>
+    <div className={cn('notice', !isNotice && 'status--delete', windowTheme ? 'notice--light' : 'notice--dark')}>
       <div className={cn('notice__inner')}>
         <div className={cn('notice__content')}>
-          <span className={cn('notice__title')}>
-            개발자를 위한 행사 정보, 데브이벤트
-          </span>
-          <Image
-            src={"/icon/notice.png"}
-            width={36}
-            height={36}
-          />
+          <span className={cn('notice__title')}>개발자를 위한 행사 정보, 데브이벤트</span>
+          <Image src={'/icon/notice.png'} width={36} height={36} />
         </div>
-        <div className={cn('icon')} onClick={handleModal}>
-          <DeleteIcon
-            color={windowTheme ? "rgba(49, 50, 52, 1)" : "rgba(203, 203, 206, 1)"}
-          />
+        <div className={cn('icon')} onClick={hideModal}>
+          <DeleteIcon color={windowTheme ? 'rgba(49, 50, 52, 1)' : 'rgba(203, 203, 206, 1)'} />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default NoticeModal
+export default NoticeModal;
