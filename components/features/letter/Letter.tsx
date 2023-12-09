062ed969d9f766163d12b9415a02701c357260f6
고언약[KR Server Tech] <covenant.ko@kakao.com>
2023-12-09 15:51:50 +0900

    #116 이메일 발송 코드 정리

--- components/features/letter/Letter.tsx
+++ components/features/letter/Letter.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image"
-import classNames from "classnames/bind"
-import style from 'components/features/letter/Letter.module.scss'
-import FillButton from "components/common/buttons/FillButton";
-import Link from "next/link";
+import FillButton from 'components/common/buttons/FillButton';
+import style from 'components/features/letter/Letter.module.scss';
+import classNames from 'classnames/bind';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const cn = classNames.bind(style);
 
 const onClick = () => {
-  return ;
-}
+  return;
+};
 
-function Letter() {
+const Letter = () => {
   return (
     <section className={cn('letter')}>
       <div className={cn('inner')}>
@@ -18,12 +18,12 @@ function Letter() {
           <div>
             <div className={cn('letter__title')}>
               매주
-              <span>데브이벤트</span> 
+              <span>데브이벤트</span>
               <br className={cn('break__line')} />
               소식을 받아보세요
             </div>
             <div className={cn('letter__desc')}>
-              일요일 저녁 8시 개발자 행사 소식을 
+              일요일 저녁 8시 개발자 행사 소식을
               <br className={cn('break__line')} />
               메일로 보내드려요
             </div>
@@ -37,19 +37,19 @@ function Letter() {
             />
           </div>
         </div>
-        <Link href={"https://github.com/brave-people/Dev-Event-Subscribe"}>
+        <Link href={'https://github.com/brave-people/Dev-Event-Subscribe'}>
           <a className={cn('button__container')} target="_blank">
-            <FillButton 
+            <FillButton
               onClick={onClick}
               label="무료 구독하기"
               color="primary"
               rounded={true}
             />
-          </a>        
+          </a>
         </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Letter
+export default Letter;
