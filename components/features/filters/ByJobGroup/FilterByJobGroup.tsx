ccad89b42c07041a2735ac3c60a8dbfb2f875472
고언약[KR Server Tech] <djsdir913@gmail.com>
2024-05-15 15:55:53 +0900

    fix: prettier 적용 | console.log 제거

--- components/features/filters/ByJobGroup/FilterByJobGroup.tsx
+++ components/features/filters/ByJobGroup/FilterByJobGroup.tsx
@@ -1,10 +1,10 @@
+import JobGroupTag from 'components/common/tag/JobGroupTag';
+import style from 'components/features/filters/ByJobGroup/FilterByJobGroup.module.scss';
+import { jobGroups } from 'components/features/filters/jobGroup';
+import { EventContext } from 'context/event';
+import { UrlContext } from 'types/Context';
 import React, { useEffect, useContext } from 'react';
 import classNames from 'classnames/bind';
-import style from 'components/features/filters/ByJobGroup/FilterByJobGroup.module.scss';
-import JobGroupTag from 'components/common/tag/JobGroupTag';
-import { jobGroups } from 'components/features/filters/jobGroup';
-import { UrlContext } from 'types/Context';
-import { EventContext } from 'context/event';
 import { useRouter } from 'next/router';
 
 const cn = classNames.bind(style);
@@ -17,15 +17,13 @@ function FilterByJobGroup({ context }: Props) {
   const router = useRouter();
   const { updateJobGroupList } = useContext(EventContext);
   const reflactTagList = () => {
-    let temp: string[] = [];
-    console.log(context);
+    const temp: string[] = [];
 
     if (context?.tagList === undefined) return;
     for (let i = 0; i < context.tagList.length; i++) {
       temp.push(decodeURIComponent(context.tagList[i]));
       updateJobGroupList(temp);
     }
-    console.log(temp);
   };
   useEffect(() => {
     if (router.asPath !== '/events') {
