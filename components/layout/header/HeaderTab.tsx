8dfcd297544f6692bb9bd09f5461683ad9864327
고언약[KR Server Tech] <covenant.ko@kakao.com>
2025-01-28 12:00:26 +0900

    [feature/humor-tab] 유머 탭 생성

---
+++ components/layout/header/HeaderTab.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames/bind';
+import { useRouter } from 'next/router';
+import styles from './HeaderTab.module.scss';
+import React, { useMemo } from 'react';
+import Link from 'next/link';
+
+const cn = classNames.bind(styles);
+
+const HeaderTab = () => {
+  return (
+    <div className={cn('container')}>
+      <Link href={'https://programmer-humor.github.io'}>
+        <div className={cn('tab')}>
+          <span className={cn('tab-text', 'active')}>개발자 유머</span>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default HeaderTab;
